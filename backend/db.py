d4156fbab8d87e0f63d292969c04dedbcecfa70e
Varshith Pabbisetty <krishnavarshith.pabbisetty@sjsu.edu>
2025-05-07 15:55:23 -0700

    PostgreSQL connection

--- backend/db.py
+++ backend/db.py
@@ -3,21 +3,27 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgres://postgres@localhost:5432/smart_equity_dev")
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql://postgres@localhost:5432/smart_equity_dev"
+)
 
 from sqlalchemy.engine.url import make_url
 
 if make_url(SQLALCHEMY_DATABASE_URL).get_backend_name() == "sqlite":
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
 else:
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-'''engine = create_engine(
+"""engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)'''
+)"""
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
